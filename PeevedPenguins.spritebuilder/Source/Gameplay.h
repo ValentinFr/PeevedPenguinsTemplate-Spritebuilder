c3350f443e8401a4560f3d90c806f6c8d9224612
Valentin <ValentinFr@MacBook-Pro-Valentin.local>
2014-07-14 13:48:04 +0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Valentin on 14.07.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
